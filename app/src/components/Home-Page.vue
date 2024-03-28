2c5151da0a32310f7bba82b502dde87124cbe2c2
Praktikant <praktikant@MBPvonPaktikant.awesome>
2024-03-28 13:27:35 +0100

    playlist future Beta

--- app/src/components/Home-Page.vue
+++ app/src/components/Home-Page.vue
@@ -1,36 +1,34 @@
 <template>
-  <div id="Home-page" >
+  <div id="Home-page">
     <div style="display: flex; justify-content: center;" class="pt-5">
       <searchbar @audio-changed="setAudioChanged" @audio="setAudio" class="z-30 fixed" />
     </div>
     <div class="mt-40 overflow-x-auto w-full h-[27rem]">
-      <table >
-      <tr>
-        <td v-for="item in serverObject" :key="item">
-          <songBanner :data-object="item" @audio-changed="setAudioChanged" @audio="setAudio" @liste="setListe" class="pt-20 pl-[3.2rem] pr-10 " />
-        </td>
-        
-          
-      </tr>
-    </table>
+      <table>
+        <tr>
+          <td v-for="item in serverObject" :key="item">
+            <songBanner :data-object="item" @audio-changed="setAudioChanged" @audio="setAudio" @liste="setListe"
+              class="pt-20 pl-[3.2rem] pr-10 " />
+          </td>
+
+
+        </tr>
+      </table>
     </div>
 
-    <audio v-if="audio" :src="audio" controls autoplay  ref="audio" @timeupdate="updateProgress" style="display: none;"
+    <audio v-if="audio" :src="audio" controls autoplay ref="audio" @timeupdate="updateProgress" style="display: none;"
       @ended="audioEnded" v></audio>
     <div class="controls">
-      <button @click="togglePlay" class="transition ease-in-out delay-150 bg-[] hover:-translate-y-1 hover:scale-110  duration-300">
+      <button @click="togglePlay"
+        class="transition ease-in-out delay-150 bg-[] hover:-translate-y-1 hover:scale-110  duration-300">
         <img src="../assets/play-button-svgrepo-com.svg" class="w-10" v-if="!isPlaying">
-        <img src="../assets/pause-button-svgrepo-com.svg" class="w-10 " v-else >
+        <img src="../assets/pause-button-svgrepo-com.svg" class="w-10 " v-else>
       </button>
       <div class="progress-bar ">
         <div class="progress" :style="{ width: progress + '%' }"></div>
       </div>
-      <input v-model="percent" type="range"
-        class="windowsSliderInput text-[#e46f03]"
-        min="0"
-        max="100"
-        @click="volumenChanger"
-      />
+      <input v-model="percent" type="range" class="windowsSliderInput text-[#e46f03]" min="0" max="100"
+        @click="volumenChanger" />
     </div>
   </div>
 </template>
@@ -51,10 +49,10 @@ export default {
       audioChange: false,
       serverObject: null,
       percent: 100,
-      liste:[],
+      liste: [],
       listeAddet: 0,
-      listeDurch:0
-      
+      listeDurch: 0
+
     }
   },
 
@@ -82,7 +80,7 @@ export default {
         audio.pause();
       }
     },
-    
+
 
     updateProgress() {
       const audio = this.$refs.audio;
@@ -92,7 +90,7 @@ export default {
     audioEnded() {
       this.isPlaying = false
       var data = []
-      data.push( window.localStorage.getItem('songs'+ this.listeDurch))
+      data.push(window.localStorage.getItem('songs' + this.listeDurch))
       this.audio = data
       this.listeDurch = this.listeDurch + 1
       console.log(this.audio);
@@ -104,32 +102,32 @@ export default {
       axios.get('http://localhost:3000/songs')
         .then(({ data }) => {
           this.serverObject = data
-          
+
         })
     },
-    volumenChanger(){
+    volumenChanger() {
       const audio = this.$refs.audio;
       audio.volume = this.percent / 100
       console.log(audio.volume)
     },
-    setListe(value){
+    setListe(value) {
       this.liste = value
       this.saveInStorage()
     },
-    saveInStorage(){
-            const data = this.liste
-            window.localStorage.setItem('songs' +  this.listeAddet, data)
-            this.listeAddet = this.listeAddet + 1
-            console.log(this.listeAddet)
-            window.localStorage.setItem('listeAddet', JSON.parse(this.listeAddet))
-        },
-        listeAddetCheck(){
-            this.listeAddet  = window.localStorage.getItem('listeAddet')
-            if(this.listeAddet == null){
-              this.listeAddet += 1
-            }
-            console.log(this.listeAddet)
-        }
+    saveInStorage() {
+      const data = this.liste
+      window.localStorage.setItem('songs' + this.listeAddet, data)
+      this.listeAddet = this.listeAddet + 1
+      console.log(this.listeAddet)
+      window.localStorage.setItem('listeAddet', JSON.parse(this.listeAddet))
+    },
+    listeAddetCheck() {
+      this.listeAddet = window.localStorage.getItem('listeAddet')
+      if (this.listeAddet == null) {
+        this.listeAddet += 1
+      }
+      console.log(this.listeAddet)
+    }
 
   },
   created() {
@@ -176,6 +174,7 @@ export default {
   border-radius: 5px;
   transition: width 0.1s;
 }
+
 ::-webkit-scrollbar {
   padding-top: 10px;
   width: 10px;
@@ -184,21 +183,23 @@ export default {
 /* Track */
 ::-webkit-scrollbar-track {
   background: #333;
-  
+
 }
+
 /* Handle */
 ::-webkit-scrollbar-thumb {
   background: #e46f03;
   border-radius: 15px;
   width: 10px;
 }
+
 ::-webkit-scrollbar-thumb :hover {
   background: #ffffff;
   border-radius: 15px;
   width: 10px;
 }
-input[type="range"] 
- {
+
+input[type="range"] {
   -webkit-appearance: none;
   background: #333;
   cursor: pointer;
@@ -210,53 +211,58 @@ input[type="range"]
 
 
 input[type="range"]::-webkit-slider-thumb {
-   -webkit-appearance: none; /* Override default look */
-   appearance: none; /* Centers thumb on the track */
-   margin-top: -12;
-   background-color: #e46f03;
-   height: 1rem;
-   width: 1rem;    
-   border-radius: 9999px;
+  -webkit-appearance: none;
+  /* Override default look */
+  appearance: none;
+  /* Centers thumb on the track */
+  margin-top: -12;
+  background-color: #e46f03;
+  height: 1rem;
+  width: 1rem;
+  border-radius: 9999px;
 }
 
-@media only screen and (max-width: 800px){
+@media only screen and (max-width: 800px) {
   .controls {
-  position: fixed;
-  bottom: 20px;
-  left: 50%;
-  transform: translateX(-50%);
-  background-color: #535353;
-  padding: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 10px;
-  width: 300px;
-}
-input[type="range"] 
- {
-  display: none;
-}
+    position: fixed;
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+    background-color: #535353;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 10px;
+    width: 300px;
+  }
+
+  input[type="range"] {
+    display: none;
+  }
 
 
-input[type="range"]::-webkit-slider-thumb {
-  display: none;
-}
-::-webkit-scrollbar {
-  display: none;
-}
+  input[type="range"]::-webkit-slider-thumb {
+    display: none;
+  }
 
-/* Track */
-::-webkit-scrollbar-track {
-  display: none;
-  
-}
-/* Handle */
-::-webkit-scrollbar-thumb {
-  display: none;
-}
-::-webkit-scrollbar-thumb :hover {
-  display: none;
-}
+  ::-webkit-scrollbar {
+    display: none;
+  }
+
+  /* Track */
+  ::-webkit-scrollbar-track {
+    display: none;
+
+  }
+
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    display: none;
+  }
+
+  ::-webkit-scrollbar-thumb :hover {
+    display: none;
+  }
 }
 </style>
